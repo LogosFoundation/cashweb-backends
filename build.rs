b8070ca38fa6bcd2bde6046359a2aee74a6f7b95
Harry Barber <harrybarber@protonmail.com>
2019-12-06 23:50:00 +0700

    Add messaging protobuf schema

--- build.rs
+++ build.rs
@@ -1,8 +1,9 @@
 fn main() {
     prost_build::compile_protos(
         &[
-            "src/proto/addressmetadata.proto",
-            "src/proto/paymentrequest.proto",
+            "src/proto/pop/addressmetadata.proto",
+            "src/proto/pop/paymentrequest.proto",
+            "src/proto/messaging/messages.proto",
         ],
         &["src/"],
     )
