d55bc6856e34093dbd5586a5297a4d760135ec04
Harry Barber <harrybarber@protonmail.com>
2020-01-13 10:08:54 +0700

    Update submodules and build script

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 fn main() {
     prost_build::compile_protos(
         &[
-            "src/proto/pop/addressmetadata.proto",
+            "src/proto/keyserver/addressmetadata.proto",
             "src/proto/pop/paymentrequest.proto",
             "src/proto/relay/messages.proto",
             "src/proto/relay/filters.proto",
