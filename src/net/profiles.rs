2a4d7d78a4816c7a14337d30a9376d7401083113
Harry Barber <harrybarber@protonmail.com>
2020-03-09 06:56:22 +0700

    Add profile signature verification

--- src/net/profiles.rs
+++ src/net/profiles.rs
@@ -4,6 +4,8 @@ use bitcoincash_addr::Address;
 use bytes::Bytes;
 use prost::Message as _;
 use rocksdb::Error as RocksError;
+use secp256k1::{key::PublicKey, Error as SecpError, Message, Secp256k1, Signature};
+use sha2::{Digest, Sha256};
 use warp::{http::Response, hyper::Body, reject::Reject};
 
 use super::IntoResponse;
@@ -13,7 +15,12 @@ use crate::{db::Database, models::wrapper::AuthWrapper};
 pub enum ProfileError {
     NotFound,
     Database(RocksError),
+    InvalidSignature(SecpError),
+    Message(SecpError),
     ProfileDecode(prost::DecodeError),
+    PublicKey(SecpError),
+    Signature(SecpError),
+    UnsupportedScheme,
 }
 
 impl From<RocksError> for ProfileError {
@@ -27,7 +34,12 @@ impl fmt::Display for ProfileError {
         let printable = match self {
             Self::NotFound => "not found",
             Self::Database(err) => return err.fmt(f),
+            Self::InvalidSignature(err) => return err.fmt(f),
+            Self::Message(err) => return err.fmt(f),
             Self::ProfileDecode(err) => return err.fmt(f),
+            Self::PublicKey(err) => return err.fmt(f),
+            Self::Signature(err) => return err.fmt(f),
+            Self::UnsupportedScheme => "unsupported signature scheme",
         };
         f.write_str(printable)
     }
@@ -40,14 +52,18 @@ impl IntoResponse for ProfileError {
         match self {
             Self::NotFound => 404,
             Self::Database(_) => 500,
-            Self::ProfileDecode(_) => 400,
+            Self::UnsupportedScheme => 501,
+            _ => 400,
         }
     }
 }
 
-pub async fn get_profile(addr: Address, database: Database) -> Result<Response<Body>, ProfileError> {
+pub async fn get_profile(
+    addr: Address,
+    database: Database,
+) -> Result<Response<Body>, ProfileError> {
     // Get profile
-    let mut profile = database
+    let profile = database
         .get_profile(addr.as_body())?
         .ok_or(ProfileError::NotFound)?;
 
@@ -64,10 +80,23 @@ pub async fn put_profile(
     profile_raw: Bytes,
     db_data: Database,
 ) -> Result<Response<Body>, ProfileError> {
-    // TODO: Do validation
-    let profile =
-        AuthWrapper::decode(profile_raw).map_err(ProfileError::ProfileDecode)?;
+    // Decode profile
+    let profile = AuthWrapper::decode(profile_raw).map_err(ProfileError::ProfileDecode)?;
 
+    // Verify signatures
+    let pubkey = PublicKey::from_slice(&profile.pub_key).map_err(ProfileError::PublicKey)?;
+    if profile.scheme != 1 {
+        // TODO: Support Schnorr
+        return Err(ProfileError::UnsupportedScheme);
+    }
+    let signature = Signature::from_compact(&profile.signature).map_err(ProfileError::Signature)?;
+    let secp = Secp256k1::verification_only();
+    let payload_digest = Sha256::new().chain(&profile.serialized_payload).result();
+    let msg = Message::from_slice(&payload_digest).map_err(ProfileError::Message)?;
+    secp.verify(&msg, &signature, &pubkey)
+        .map_err(ProfileError::InvalidSignature)?;
+
+    // Put to database
     db_data.put_profile(addr.as_body(), &profile.serialized_payload)?;
 
     // Respond
